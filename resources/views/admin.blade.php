2c59d999c7495079eca8cb82b0e5786f04bf3e74
HRawat8630 <rawathimesh03@gmail.com>
2025-12-20 18:14:54 +0530

    pass data in navbar component

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -11,17 +11,7 @@
 
 <body class=" bg-gray-200">
 
-    <nav class="bg-gray-100 shadow-lg px-5 py-5 m-2">
-        <div class="flex justify-between item-center">
-            <div class="font-bold text-[20px] hover:text-green-500">Quiz System</div>
-            <div class="flex gap-5">
-                <a class="text-gray-700 hover:text-blue-500" href="/admin-categories">Categories</a>
-                <a class="text-gray-700 hover:text-blue-500" href="">Quiz</a>
-                <a class="text-gray-700 hover:text-blue-500" href="">Wellcome,{{$name}}</a>
-                <a class="text-gray-700 hover:text-blue-500" href="">Logout</a>
-            </div>
-        </div>
-    </nav>
+   <x-navbar name={{$name}}> </x-navbar>
 </body>
 
 </html>
